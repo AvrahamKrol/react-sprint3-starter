4888a34d3c80e11f360c8bf93fc5c9712733422d
Avraham Krol <avrahamkrol@gmail.com>
2026-08-20 15:03:31 +0300

    fix: improve modal behavior and update overflow handling in NoteList

--- apps/note/cmps/Modal.jsx
+++ apps/note/cmps/Modal.jsx
@@ -4,8 +4,17 @@ export function Modal({ isShown, children, style, onCloseModal = null }) {
   const dialogRef = useRef(null)
 
   useEffect(() => {
-    if (isShown && dialogRef.current) dialogRef.current.showModal()
-    else if (dialogRef.current) dialogRef.current.close()
+    if (isShown && dialogRef.current) {
+      dialogRef.current.showModal()
+      document.body.style.overflow = 'hidden'
+    } else if (dialogRef.current) {
+      dialogRef.current.close()
+      document.body.style.overflow = ''
+    }
+
+    return () => {
+      document.body.style.overflow = ''
+    }
   }, [isShown])
 
   function onClose() {
